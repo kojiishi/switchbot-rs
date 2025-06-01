f90a889ec7e33892d3057ccb5d56c3adb4024557
Koji Ishii <kojii@chromium.org>
2025-06-01 14:02:22 +0900

    Fix handling SwitchBot API command error
    * Support cases when the response doesn't have a `body`.
    * Add the `SwitchBotError` to pass `status_code` and `message`
      from the SwitchBot API server.

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -20,5 +20,5 @@ pub use device_list::*;
 mod switch_bot;
 pub use switch_bot::*;
 mod switch_bot_service;
-pub use switch_bot_service::CommandRequest;
 pub(crate) use switch_bot_service::*;
+pub use switch_bot_service::{CommandRequest, SwitchBotError};
