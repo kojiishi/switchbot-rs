ea5b0e5b6695d9339e055fca4c0e32561ba7441d
Koji Ishii <kojii@chromium.org>
2025-06-05 02:49:11 +0900

    Adjust log output for clarity (#20)

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,18 +1,32 @@
+use std::io::Write;
+
 use switchbot_cli::Cli;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    if std::env::var("RUST_LOG").is_ok() {
-        env_logger::init();
-    } else {
-        env_logger::Builder::new()
-            .format_timestamp(None)
-            .format_target(false)
-            .filter_level(log::LevelFilter::Info)
-            .init();
-    }
+    init_logger();
 
     let mut cli = Cli::new_from_args();
     cli.run().await?;
     Ok(())
+}
+
+fn init_logger() {
+    // If `RUST_LOG` is set, initialize the `env_logger` in its default config.
+    if std::env::var("RUST_LOG").is_ok() {
+        env_logger::init();
+        return;
+    }
+
+    env_logger::Builder::new()
+        .filter_level(log::LevelFilter::Info)
+        .format(|buf, record| match record.level() {
+            log::Level::Info => writeln!(buf, "{}", record.args()),
+            _ => {
+                let style = buf.default_level_style(record.level());
+                writeln!(buf, "{style}{}{style:#}: {}", record.level(), record.args())
+            }
+        })
+        .target(env_logger::Target::Stdout)
+        .init();
 }
