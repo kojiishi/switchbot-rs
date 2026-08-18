c67924532d5b0408970c1e9381da5bc075917240
Koji Ishii <kojii@chromium.org>
2026-08-18 19:51:36 +0900

    feat: use JSON data for help

--- api/src/markdown.rs
+++ api/src/markdown.rs
@@ -9,9 +9,16 @@ use regex::Regex;
 /// # use switchbot_api::Markdown;
 /// assert_eq!(Markdown::new("a<br>b").to_string(), "a\nb");
 /// ```
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug, Default, serde::Deserialize)]
+#[serde(from = "String")]
 pub struct Markdown {
     markdown: String,
+}
+
+impl From<String> for Markdown {
+    fn from(markdown: String) -> Self {
+        Self { markdown }
+    }
 }
 
 impl Markdown {
@@ -26,31 +33,10 @@ impl Markdown {
         &self.markdown
     }
 
-    pub(crate) fn em(text: &str) -> Option<&str> {
-        const RE_EM_PAT: &str = r"\*([^*]+)\*";
-        static RE_EM: LazyLock<Regex> = LazyLock::new(|| Regex::new(RE_EM_PAT).unwrap());
-        if let Some(captures) = RE_EM.captures(text) {
-            return captures.get(1).map(|m| m.as_str());
-        }
-        None
-    }
-
     fn plain_text(&self) -> String {
         const RE_BR_PAT: &str = r"(?i)<br\s*/?>";
         static RE_BR: LazyLock<Regex> = LazyLock::new(|| Regex::new(RE_BR_PAT).unwrap());
         RE_BR.replace_all(&self.markdown, "\n").into()
-    }
-
-    pub(crate) fn table_columns(line: &str) -> Option<Vec<&str>> {
-        if line.starts_with('|') && line.ends_with('|') {
-            let columns = line
-                .split_terminator('|')
-                .skip(1)
-                .map(|s| s.trim())
-                .collect::<Vec<&str>>();
-            return Some(columns);
-        }
-        None
     }
 }
 
@@ -63,21 +49,6 @@ impl Display for Markdown {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn em() {
-        assert_eq!(Markdown::em("*a*"), Some("a"));
-        assert_eq!(Markdown::em("x*a*x"), Some("a"));
-
-        assert_eq!(Markdown::em("a"), None);
-        assert_eq!(Markdown::em("*a"), None);
-        assert_eq!(Markdown::em("a*"), None);
-
-        assert_eq!(
-            Markdown::em("device type. *Hub*, *Hub Plus*, *Hub Mini*, *Hub 2* or *Hub 3*."),
-            Some("Hub")
-        );
-    }
 
     fn to_plain_text(markdown: &str) -> String {
         Markdown::new(markdown).plain_text()
@@ -95,16 +66,5 @@ mod tests {
         assert_eq!(to_plain_text("a<br>b"), "a\nb");
 
         assert_eq!(to_plain_text("a<br>b<br>c"), "a\nb\nc");
-    }
-
-    fn to_table_columns(line: &str) -> Option<Vec<&str>> {
-        Markdown::table_columns(line)
-    }
-
-    #[test]
-    fn table_columns() {
-        assert_eq!(to_table_columns("1|2|3"), None);
-        assert_eq!(to_table_columns("|1|2|3|"), Some(vec!["1", "2", "3"]));
-        assert_eq!(to_table_columns("| 1 | 2 | 3 |"), Some(vec!["1", "2", "3"]));
     }
 }
